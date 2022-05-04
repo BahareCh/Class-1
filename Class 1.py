cdf25305e711d707fe60f5af707642d7dacdf516
BahareCh <94406774+BahareCh@users.noreply.github.com>
2022-05-03 20:14:15 -0400

    Update Class 1.py

--- Class 1.py
+++ Class 1.py
@@ -1,7 +1,8 @@
-File1 = int( input ("Enter the input1:"))
-Enter the input1: 5
->>> File2 = int(input ("Enter the input2:"))
-Enter the input2: 7
->>> A= File1+ File2
->>> Print (A)
-
+A = int( input ("input number 1:"))
+B = int(input ("input number 2:"))
+output 1 = A+B
+Print (output 1)
+output 2 = A-B
+Print (output 2)
+output 3 = A**B
+Print (output 3)
