2cdbea18dea1f907759166ad486570a3adadbb96
Mohsen <68563395+MohsenMozafarigh@users.noreply.github.com>
2022-05-03 20:31:45 -0400

    Update Class 1.py

--- Class 1.py
+++ Class 1.py
@@ -10,3 +10,12 @@ output_4 = A/B
 print (output_4)
 output_5= A*B
 print (output_5)
+
+C = int(input("input number 2:"))
+
+new_output_1 = output_1 + C
+new_output_2 = output_2 - C
+new_output_3 = output_3 ** C
+new_output_4 = output_4 / C
+new_output_5 = output_1 * C
+print (new_output_1, new_output_2, new_output_3, new_output_4, new_output_5)
