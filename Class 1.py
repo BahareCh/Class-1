55f510c3722b2f9120994ac16457b10a77ad6115
BahareCh <94406774+BahareCh@users.noreply.github.com>
2022-05-03 20:28:33 -0400

    Update Class 1.py

--- Class 1.py
+++ Class 1.py
@@ -1,12 +1,12 @@
 A = int( input ("input number 1:"))
 B = int(input ("input number 2:"))
 output_1 = A+B
-Print (output_1)
+print (output_1)
 output_2 = A-B
-Print (output_2)
+print (output_2)
 output_3 = A**B
-Print (output_3)
+print (output_3)
 output_4 = A/B
-Print (output_4)
+print (output_4)
 output_5= A*B
-Print (output_5)
+print (output_5)
